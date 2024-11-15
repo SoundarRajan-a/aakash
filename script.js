28effb335c20ed670892d6cfb8302280f8450d97
SoundarRajan-a <soundarrajan2509@gmail.com>
2024-11-15 20:16:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 "use strict";
+
+// Getting DOM elements
 let text = document.getElementById('text');
 let bird1 = document.getElementById('bird1');
 let bird2 = document.getElementById('bird2');
@@ -7,8 +9,13 @@ let rocks = document.getElementById('rocks');
 let forest = document.getElementById('forest');
 let water = document.getElementById('water');
 let header = document.getElementById('header');
+
+// Scroll event listener for animations
 window.addEventListener('scroll', function () {
+    // Get the current scroll value
     let value = window.scrollY;
+
+    // Update positions of elements based on scroll
     text.style.top = 50 + value * -.1 + '%';
     bird2.style.top = value * -1.5 + 'px';
     bird2.style.left = value * 2 + 'px';
